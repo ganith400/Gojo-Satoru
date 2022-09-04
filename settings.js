9bd819e93e9162b6731729a9729328b17c6a0427
ganith400 <112805337+ganith400@users.noreply.github.com>
2022-09-04 17:05:32 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,19 +19,19 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['+9455988946']
+global.premium = ['+9455988946']
+global.ownernomer = '+9455988946'
+global.ownername = '🐦ganith'
+global.botname = 'ganith'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
+global.packname = 'ganith'
 global.author = '🐦Star Boy Nexus'
-global.sessionName = 'session'
+global.sessionName = 'ganith'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
