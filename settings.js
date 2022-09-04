4cad61f8959ca39200c9306a9b5919b845c47ace
ganith400 <112805337+ganith400@users.noreply.github.com>
2022-09-04 16:44:44 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,9 +19,9 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
+global.owner = ['+9455988946']
+global.premium = ['+9455988946']
+global.ownernomer = '+9455988946'
 global.ownername = '🐦Star Boy Nexus'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
